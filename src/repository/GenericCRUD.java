eae86c1dcef7ef08461b7847079a292adf00e62b
LucasLouzano <120424781+LucasLouzano@users.noreply.github.com>
2023-10-10 11:41:32 -0300

    Update GenericCRUD.java

--- src/repository/GenericCRUD.java
+++ src/repository/GenericCRUD.java
@@ -7,7 +7,9 @@ public interface GenericCRUD<T, v> {
     void create(T parametro);
 
     List<T> readAll();
+    
     boolean update(v nome,T parametro);
+    
     boolean delete(String nome);
 
 }
