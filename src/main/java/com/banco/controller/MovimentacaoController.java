243ffb1a4beb1541a3db251a8660480db949c5c8
Lucas <lucaslouzano55@gmail.com>
2025-08-11 23:29:42 -0300

    Implementação class Movimentação controller|

--- src/main/java/com/banco/controller/MovimentacaoController.java
+++ src/main/java/com/banco/controller/MovimentacaoController.java
@@ -1,2 +1,31 @@
-package com.banco.controller;public class MovimentacaoController {
+package com.banco.controller;
+
+import com.banco.model.Movimentacao;
+import com.banco.service.MovimentacaoService;
+import com.banco.service.impl.MovimentacaoServiceImpl;
+
+import java.util.List;
+
+public class MovimentacaoController{
+    private final MovimentacaoService service = new MovimentacaoServiceImpl();
+
+    public void create(Movimentacao movimetacao) {
+        service.create(movimetacao);
+
+    }
+    public List<Movimentacao> readAll() {
+        return service.readAll();
+    }
+
+    public Movimentacao read(Long id) {
+        return service.read(id);
+    }
+
+    public boolean update(Long id, Movimentacao movimentacao) {
+        return service.update(id,movimentacao);
+    }
+
+    public boolean delete(Long id) {
+        return service.delete(id);
+    }
 }
