aec041086bfd87f0c7b955a6ada605ca3b61e406
Lucas <lucaslouzano55@gmail.com>
2025-08-11 23:23:20 -0300

    Implementação do movimentação DAO

--- src/main/java/com/banco/repository/MovimentacaoDAO.java
+++ src/main/java/com/banco/repository/MovimentacaoDAO.java
@@ -2,5 +2,9 @@ package com.banco.repository;
 
 import com.banco.model.Movimentacao;
 
-public interface MovimentacaoRepository extends GenericCRUD<Movimentacao, Long>{
+import java.util.Date;
+import java.util.List;
+
+public interface MovimentacaoDAO extends GenericCRUD<Movimentacao, Long> {
+    List<Movimentacao> buscarPorContaEPeriodo(String numeroConta, Date inicio, Date fim);
 }
