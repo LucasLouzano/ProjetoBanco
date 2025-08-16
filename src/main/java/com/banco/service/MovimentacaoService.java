42e4c27d531696de5611ea6e7835b2b784af604b
Lucas <lucaslouzano55@gmail.com>
2025-08-16 14:21:03 -0300

    Implementação do MovimentaçãoService

--- src/main/java/com/banco/service/MovimentacaoService.java
+++ src/main/java/com/banco/service/MovimentacaoService.java
@@ -1,2 +1,28 @@
-package com.banco.service;public interface MovimentacaoService {
+package com.banco.service;
+
+import com.banco.exceptions.ContaException;
+import com.banco.model.Conta;
+import com.banco.model.Movimentacao;
+
+import java.util.Date;
+import java.util.List;
+
+public interface MovimentacaoService {
+    void create(Movimentacao movimetacao) throws ContaException;
+
+    List<Movimentacao> readAll();
+
+    Movimentacao read(Long id);
+
+    boolean update(Long id, Movimentacao movimentacao);
+
+    boolean delete(Long id);
+
+    List<Movimentacao> extratoPorPeriodo(String numeroConta, Date inicio, Date fim);
+
+    List<Movimentacao> extratoGeralPorConta();
+
+    List<Movimentacao> buscarConta(String numeroConta);
+
+
 }
